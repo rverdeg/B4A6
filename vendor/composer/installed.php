21f22eb0ab41bef7a621e70955707223ad269513
Rene V <reneverdeg@hotmail.com>
2024-10-24 18:05:34 -0600

    Initial Commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7a8b59738046a7c64c0e93f6e4a9e9f3428b738a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7a8b59738046a7c64c0e93f6e4a9e9f3428b738a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'parse/php-sdk' => array(
+            'pretty_version' => '1.5.1',
+            'version' => '1.5.1.0',
+            'reference' => '2f3984f52b548f176931c9ddc7a65b3e03304b6a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../parse/php-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
